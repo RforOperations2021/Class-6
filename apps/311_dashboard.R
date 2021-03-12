ce431eaffa614adc6f814f89fe3331140f4aeddc
Geoffrey Arnold <geoffrey.lloyd.arnold@gmail.com>
2021-03-11 20:36:32 -0500

    Fix space error

--- apps/311_dashboard.R
+++ apps/311_dashboard.R
@@ -2,13 +2,14 @@
 
 library(shiny)
 library(httr)
+library(dplyr)
 library(jsonlite)
 library(plotly)
 library(htmltools)
 
 ckanSQL <- function(url) {
   # Make the Request
-  r <- RETRY("GET", URLencode(url))
+  r <- RETRY("GET", URLencode(url), repeated = TRUE)
   # Extract Content
   c <- content(r, "text")
   # Basic gsub to make NA's consistent with R
@@ -19,8 +20,8 @@ ckanSQL <- function(url) {
 
 # Unique values for Resource Field
 ckanUniques <- function(id, field) {
-  url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22")
-  c(ckanSQL(URLencode(url)))
+  url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20DISTINCT(%22", field, "%22)%20from%20%22", id, "%22%20WHERE%20%22CREATED_ON%22%20BETWEEN%20%27", Sys.Date()-30, "%27%20AND%20%27", Sys.Date(), "%27")
+  ckanSQL(url)
 }
 
 types <- sort(ckanUniques("76fda9d0-69be-4dd5-8108-0de7907fc5a4", "REQUEST_TYPE")$REQUEST_TYPE)
@@ -60,8 +61,11 @@ ui <- fluidPage(
 # Define server logic
 server <- function(input, output) {
    load311 <- reactive({
+     req(input$type_select)
      # Build API Query with proper encodes
      url <- paste0("https://data.wprdc.org/api/action/datastore_search_sql?sql=SELECT%20*%20FROM%20%2276fda9d0-69be-4dd5-8108-0de7907fc5a4%22%20WHERE%20%22CREATED_ON%22%20%3E=%20%27", input$dates[1], "%27%20AND%20%22CREATED_ON%22%20%3C=%20%27", input$dates[2], "%27%20AND%20%22REQUEST_TYPE%22%20=%20%27", input$type_select, "%27")
+     
+     print(url)
      
      # Load and clean data
      dat311 <- ckanSQL(url) %>%
